79e8a404bab48c924e0076ef7d57adf659af88a1
bilgehancekin <shadowking_17@hotmail.com>
2013-12-03 14:59:40 +0200

    User Control
    User Control blokları girilmeye baslandı.

---
+++ bilgimyok.com/UserControl/gazete.ascx
@@ -0,0 +1,7 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="gazete.ascx.cs" Inherits="bilgimYok.UserControl.gazete" %>
+<asp:DataList ID="dtlSlider" runat="server" BorderStyle=None RepeatColumns="3" 
+    RepeatDirection="Vertical" >
+    <ItemTemplate>
+    <li><a href="#"><%#Eval("gazete_ismi") %></a></li>
+    </ItemTemplate>
+    </asp:DataList>
---
+++ bilgimyok.com/UserControl/gazete.ascx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace bilgimYok.UserControl
+{
+    public partial class gazete : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            bilgimYokEntities context = new bilgimYokEntities();
+            List<bilgimYok.gazete> listGazete = context.gazetes.ToList();
+            if (listGazete!=null && listGazete.Count>0)
+            {
+                dtlSlider.DataSource = listGazete;
+                dtlSlider.DataBind();
+            }
+
+        }
+    }
+}
