c27fa558a0fc98a99c47f52ed5f59ee396cedda9
Elif BAYRAKDAR <elifbyrkdr@gmail.com>
2013-12-03 15:12:43 +0200

    User Control
    User Controllere kategori kısmı eklendi.

---
+++ bilgimyok.com/UserControl/kategori.ascx
@@ -0,0 +1,7 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="kategori.ascx.cs" Inherits="bilgimYok.UserControl.kategori" %>
+<asp:DataList ID="dtlSlider" runat="server" BorderStyle="None"  RepeatDirection="Horizontal">
+    <ItemTemplate>
+        <li><a href="#">
+            <%#Eval("kategori_ismi") %></a> </li>
+    </ItemTemplate>
+</asp:DataList>
---
+++ bilgimyok.com/UserControl/kategori.ascx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace bilgimYok.UserControl
+{
+    public partial class kategori : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            bilgimYokEntities context = new bilgimYokEntities();
+            List<bilgimYok.kategori> ListKategori = context.kategoris.ToList();
+            if (ListKategori != null && ListKategori.Count > 0)
+            {
+                dtlSlider.DataSource = ListKategori;
+                dtlSlider.DataBind();
+                
+            }
+        }
+    }
+}
